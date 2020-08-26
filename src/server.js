6b74a3b5b16836f747f6a2813c33901ba23ff197
asugar13 <asier.ugarteche@gmail.com>
2020-08-26 16:30:53 -0400

    basic server route running

---
+++ src/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+
+// var user_routes = require('../routes/user_routes');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
+var session = require('cookie-session');
+
+var app = express();
+
+app.use(session({secret: "Shh, its a secret!"}));
+//app.use(express.static(__dirname + '/../public'));
+
+app.use(express.static(__dirname + '/assets/scripts'));
+app.use(express.static(__dirname + '/assets/css'));
+
+//new line
+app.use(express.static(__dirname + '/../public'));
+
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname +'/../public');
+app.set('view engine', 'html');
+
+// The request body is received on GET or POST.
+// A middleware that just simplifies things a bit.
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
+// Get the index page:
+app.get('/', function(req, res) {
+        res.render('welcome', {  // Note that .html is assumed.
+        errors: ''
+    });
+});
+
+// Start the server
+app.listen(3000);
+console.log('Listening on port 3000');
